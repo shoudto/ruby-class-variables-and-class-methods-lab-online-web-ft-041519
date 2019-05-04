3439374fbd0e2ab51f022d0133fa1b82d79a558c
Oscar Shoudt <shoudto@gmail.com>
2019-05-04 00:46:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,8 @@ class Song
     genre_hash = {}
     @@genres.each do |genre|
       binding.pry 
-     if genre_hash[genre] = 1    
+     if !!genre_hash[genre]
+       genre_hash[genre] += 1 
     end
     genre_hash
   end 
