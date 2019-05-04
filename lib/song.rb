ce5b88139296c020054f6eee79ad9bd371a7c550
Oscar Shoudt <shoudto@gmail.com>
2019-05-04 00:58:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,6 +42,15 @@ class Song
     genre_hash
   end 
   
-  
+  def self.artist_count 
+    artist_hash = {}
+    @@artists.each do |artist|
+      if !!artist_hash[artist]
+        artist_hash[artist] += 1 
+      else 
+        artist_hash[artist] = 1 
+      end 
+    end 
+  end 
 
 end 
